087bcde46343007b164e85fb86038ab397a780b7
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-13 16:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ for tnum in range(10):num.append(str(tnum))
 ts=[];s=[]
 print("true")
 while 1:
-    try:
-        p+=1
-        if c[p]=='"':
-            while c[p+1]!='"':p+=1;ts.append(c[p])
-            ts.reverse();p+=1
-            while len(ts)>1:ts.append(ts.pop()+ts.pop())
-            s.append(ts.pop())
-        elif c[p]=='.':print(s.pop())
-        elif c[p]in num:s.append(int(c[p]))
-        elif c[p]==',':s.append(input())
-    except IndexError:break
+ try:
+  p+=1
+  if c[p]=='"':
+   while c[p+1]!='"':p+=1;ts.append(c[p])
+   ts.reverse();p+=1
+   while len(ts)>1:ts.append(ts.pop()+ts.pop())
+   s.append(ts.pop())
+  elif c[p]=='.':print(s.pop())
+  elif c[p]in num:s.append(int(c[p]))
+  elif c[p]==',':s.append(input())
+ except IndexError:break
