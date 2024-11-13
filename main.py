ade9c76f8279231e471fe74f2459135b66585a67
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-14 00:35:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-i='''''';p=-1
-b=i.split("\n");tp=0;c=b[0];num=[]
+i="""""";p=-1
+tp=0;num=[];c="";po=0
 for tnum in range(10):num.append(str(tnum))
 ts=[];s=[]
 print("true")
 while 1:
- try:
-  p+=1
-  if c[p]=='"':
-   while c[p+1]!='"':p+=1;ts.append(c[p])
-   ts.reverse();p+=1
-   while len(ts)>1:ts.append(ts.pop()+ts.pop())
-   s.append(ts.pop())
-  elif c[p]=='.':print(s.pop())
-  elif c[p]in num:s.append(int(c[p]))
-  elif c[p]==',':s.append(input())
-  elif c[p]==':':ts.append(s.pop());ts.append(ts[0]);s.append(ts.pop());s.append(ts.pop())
- except IndexError:break
+    try:
+        p+=1
+        if i[p]=='"':
+            while i[p+1]!='"':p+=1;ts.append(i[p])
+            ts.reverse();p+=1
+            while len(ts)>1:ts.append(ts.pop()+ts.pop())
+            s.append(ts.pop())
+        elif i[p]=='.':print(s.pop(),end="")
+        elif i[p]in num:s.append(int(i[p]))
+        elif i[p]==',':c=input()
+        elif i[p]==':':ts.append(s.pop());ts.append(ts[0]);s.append(ts.pop());s.append(ts.pop())
+        elif i[p]=='}':
+            if s[0]==s[1]:
+                while i[p]!="{":p-=1
+        elif i[p]=="'":s.append(c[po]);po+=1
+    except IndexError:break
