91494ba16ea173b0f60ef75145326a2feeeb28a7
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-27 22:00:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 i=''''''
 p=-1;targ="";tp=0;c="";sub=0
-ts=[];s=[];ls={}
-codepage='"0123456789\'.,:-+`»;('
+ts=[];s=[];ld={}
+cp='"0123456789\'.,:-+`»;'
 print("true")
 while 1:
  try:
@@ -31,7 +31,7 @@ while 1:
    while i[p+1]!=")":ts.append(i[p+1]);p+=1
    ts.reverse()
    while len(ts)>1:ts.append(ts.pop()+ts.pop())
-   if i[p+3] not in codepage:ls[i[p+3]]=ts[-1]
+   if i[p+3]not in cp and i[p+4]==" ":ld[i[p+3]]=ts[0]
    else:print("Cannot overwrite built-in");break
   if len(i)-1==p:
    while 1:
